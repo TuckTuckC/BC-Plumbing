5af78604caf7abb41695fa3d5d1f749672e72aac
Tucker Craig <tuckermcraig@gmail.com>
2024-05-22 19:50:03 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Home from "./routes/Home";
+
+function App() {
+  return (
+    <div className="App border border-black">
+      APP
+    <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='border border-black h-10'>Home</div>
+  )
+}
+
+export default Home
